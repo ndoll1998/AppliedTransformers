e476c5482ce033ebe2197be80e8506b66efe7c40
Niclas Doll <niclas@amazonis.net>
2021-01-24 01:03:21 +0100

    German Yelp Dataset for Entity Classification

--- applied/tasks/ec/datasets/__init__.py
+++ applied/tasks/ec/datasets/__init__.py
@@ -3,3 +3,7 @@ from .semEval2014Task4 import (
     SemEval2014Task4_Laptops,
     SemEval2014Task4_Restaurants
 )
+from .germYelp import (
+    GermYelp_OpinionPolarity,
+    GermYelp_AspectPolarity
+)
